0a29f617ee63828c67552f12944e14aae33ded3e
Beno Suriano <bjsuriano@gmail.com>
2021-11-05 15:28:53 -0400

    Initial Commit

---
+++ Hacker Newzzz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hacker Newzzz
+//
+//  Created by Beno Suriano on 5/Nov/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hacker NewzzzTests/Hacker_NewzzzTests.swift
@@ -0,0 +1,33 @@
+//
+//  Hacker_NewzzzTests.swift
+//  Hacker NewzzzTests
+//
+//  Created by Beno Suriano on 5/Nov/21.
+//
+
+import XCTest
+@testable import Hacker_Newzzz
+
+class Hacker_NewzzzTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

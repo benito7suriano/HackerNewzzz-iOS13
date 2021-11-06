cdba53c859cc6a3d906d676b73147edc0857ee91
Beno Suriano <bjsuriano@gmail.com>
2021-11-06 09:15:14 -0400

    Converts each cell into a link to display webpage of post.

---
+++ Hacker Newzzz/View/DetailView.swift
@@ -0,0 +1,23 @@
+//
+//  DetailView.swift
+//  Hacker Newzzz
+//
+//  Created by Beno Suriano on 6/Nov/21.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    
+    let url: String?
+    
+    var body: some View {
+        WebView(urlString: url)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(url: "https://www.google.com")
+    }
+}
---
+++ Hacker Newzzz/View/WebView.swift
@@ -0,0 +1,28 @@
+//
+//  WebView.swift
+//  Hacker Newzzz
+//
+//  Created by Beno Suriano on 6/Nov/21.
+//
+
+import Foundation
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    
+    let urlString: String?
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let safeString = urlString {
+            if let url = URL(string: safeString) {
+                let request = URLRequest(url: url)
+                uiView.load(request)
+            }
+        }
+    }
+}
